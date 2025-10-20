28259bcf31f9ace18c34510348b8f0bb1aa905d4
andyintheden <andreas4242@icloud.com>
2025-10-20 17:55:47 +0200

    added entries array

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-document.getElementById('scrollBtn').addEventListener('click',scrollToNext);
+document.getElementById('scrollDown').addEventListener('click',scrollToNext);
 
 function scrollToNext() {
     document.getElementById('entries-section').scrollIntoView(
         {behavior: "smooth"}
     );
 }
+
+document.getElementById('scrollUp').addEventListener('click',scrollToForm)
+
+function scrollToForm() {
+    document.getElementById('entry-section').scrollIntoView({behavior:"smooth"});
+}
+
